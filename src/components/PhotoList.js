0e07a2bfc19423b5f6454d4cf556f7b7489158cb
Kevin0115 <kevin.choi0115@gmail.com>
2018-11-22 18:11:32 -0800

    compressed images

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,50 +1,20 @@
-import p1 from '../images/photography/slide (1).jpg';
-import p2 from '../images/photography/slide (2).jpg';
-import p3 from '../images/photography/slide (3).jpg';
-import p4 from '../images/photography/slide (4).jpg';
-import p5 from '../images/photography/slide (5).jpg';
-import p6 from '../images/photography/slide (6).jpg';
-import p7 from '../images/photography/slide (7).jpg';
-import p8 from '../images/photography/slide (8).jpg';
-import p9 from '../images/photography/slide (9).jpg';
-import p10 from '../images/photography/slide (10).jpg';
-import p11 from '../images/photography/slide (11).jpg';
-import p12 from '../images/photography/slide (12).jpg';
-import p13 from '../images/photography/slide (13).jpg';
-import p14 from '../images/photography/slide (14).jpg';
-import p15 from '../images/photography/slide (15).jpg';
-import p16 from '../images/photography/slide (16).jpg';
-import p17 from '../images/photography/slide (17).jpg';
-import p18 from '../images/photography/slide (18).jpg';
-import p19 from '../images/photography/slide (19).jpg';
-import p20 from '../images/photography/slide (20).jpg';
-import p21 from '../images/photography/slide (21).jpg';
-import p22 from '../images/photography/slide (22).jpg';
-
-// export const PhotoList = [
-//   p1,
-//   p22,
-//   p2,
-//   p3,
-//   p4,
-//   p5,
-//   // p6,
-//   p7,
-//   p8,
-//   // p9,
-//   p10,
-//   p11,
-//   p12,
-//   p13,
-//   p14,
-//   p15,
-//   p16,
-//   p17,
-//   p18,
-//   p19,
-//   p20,
-//   p21,
-// ];
+import p1 from '../images/photocompressed/slide (1).jpg';
+import p2 from '../images/photocompressed/slide (2).jpg';
+import p3 from '../images/photocompressed/slide (3).jpg';
+import p4 from '../images/photocompressed/slide (4).jpg';
+import p5 from '../images/photocompressed/slide (5).jpg';
+import p6 from '../images/photocompressed/slide (6).jpg';
+import p7 from '../images/photocompressed/slide (7).jpg';
+import p8 from '../images/photocompressed/slide (8).jpg';
+import p9 from '../images/photocompressed/slide (9).jpg';
+import p10 from '../images/photocompressed/slide (10).jpg';
+import p11 from '../images/photocompressed/slide (11).jpg';
+import p12 from '../images/photocompressed/slide (12).jpg';
+import p13 from '../images/photocompressed/slide (13).jpg';
+import p14 from '../images/photocompressed/slide (14).jpg';
+import p15 from '../images/photocompressed/slide (15).jpg';
+import p16 from '../images/photocompressed/slide (16).jpg';
+import p17 from '../images/photocompressed/slide (17).jpg';
 
 export const PhotoList = [
   {original: p1},
@@ -52,10 +22,10 @@ export const PhotoList = [
   {original: p3},
   {original: p4},
   {original: p5},
-  // {original: p6},
+  {original :p6},
   {original: p7},
   {original: p8},
-  // {original: p9},
+  {original: p9},
   {original: p10},
   {original: p11},
   {original: p12},
@@ -64,9 +34,4 @@ export const PhotoList = [
   {original: p15},
   {original: p16},
   {original: p17},
-  {original: p18},
-  {original: p19},
-  {original: p20},
-  {original: p21},
-  {original: p22},
 ];
